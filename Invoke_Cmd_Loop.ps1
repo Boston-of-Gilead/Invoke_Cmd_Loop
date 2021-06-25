0bf14003989df5dbad5af5deca7fc2d957fed04f
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-25 10:27:10 -0400

    Add files via upload

---
+++ Invoke_Cmd_Loop.ps1
@@ -0,0 +1,30 @@
+﻿#Loops an Invoke-Command down a list of machines
+
+$list = Get-Content -Path "c:\path_to\input.txt"
+$output = "c:\path_to\output.txt"
+
+forEach ($i in $list){
+    try{
+    #$info = systeminfo /s $i
+    Write-host $i
+    invoke-command -ComputerName $i -ScriptBlock {systeminfo}
+    }
+    catch {#[System.Management.Automation.RemoteException]{
+    Write-host "Error"
+    add-content $file "Machine $i not found"
+    Continue
+    }
+    #add-content $file $info    
+} 
+
+#try {
+#    foreach ($i in $list){
+#    $info = invoke-command -ComputerName $i -ScriptBlock {systeminfo}
+#    add-content $output $info 
+#    }
+#}
+#catch {
+#    #Write-host "Error"
+#    continue
+#}
+
